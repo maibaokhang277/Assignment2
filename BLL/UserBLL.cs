efc7dd1602f74f911a87a7d4bab39d858a27291a
maibaokhang277 <langkang2707@gmail.com>
2026-04-29 16:07:06 +0700

    Add files via upload

---
+++ BLL/UserBLL.cs
@@ -0,0 +1,17 @@
+﻿using DAL;
+
+namespace BLL
+{
+    public class UserBLL
+    {
+        private UserDAL userDAL = new UserDAL();
+
+        public bool Authenticate(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+                return false;
+
+            return userDAL.CheckLogin(username, password);
+        }
+    }
+}
---
+++ DAL/UserDAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class UserDAL
+    {
+        private string connString = ConfigurationManager.ConnectionStrings["MyConn"].ConnectionString;
+
+        public bool CheckLogin(string username, string password)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                string query = "SELECT COUNT(*) FROM [Users] WHERE UserName = @user AND password = @pass AND [lock] = 0";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@user", username);
+                cmd.Parameters.AddWithValue("@pass", password);
+
+                conn.Open();
+                int result = (int)cmd.ExecuteScalar();
+                return result > 0;
+            }
+        }
+    }
+}
